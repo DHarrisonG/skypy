758eecbf27c8e59ea74443a69f406a54d35a7507
github username <github email address>
2020-06-17 17:56:08 -0500

    seting up

---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import React from "./node_modules/react";
+import "./Main.css";
+
+const Main = () => {
+    return <main className="Main">Main Stuff</main>
+};
+
+export default Main; 
---
+++ src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "./node_modules/react";
+import "./Sidebar.css"
+
+const Sidebar = () => {
+    return <aside className="Sidebar">Sidebar</aside>
+}
+
+export default Sidebar;
