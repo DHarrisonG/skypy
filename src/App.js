3f353a2ce3bef1bb42370311909b9e80b67318ff
github username <github email address>
2020-06-23 09:30:34 -0500

    wut

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+import store from './store'
 import Main from "./components/Main"
 import Sidebar from "./components/Sidebar"
+import _ from 'lodash';
 
 const App = () => {
+  const { contacts } = store.getState();
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={_.values(contacts)}/>
       <Main />
     </div>
   );
